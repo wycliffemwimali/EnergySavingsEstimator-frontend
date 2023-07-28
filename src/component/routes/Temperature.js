e469c6581874c348ef22ef980db554c642e737fa
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-07-28 15:12:39 +0300

    temp data

--- src/component/routes/Temperature.js
+++ src/component/routes/Temperature.js
@@ -1,16 +1,66 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Container, TextField, Button, Typography, Box, Paper } from '@mui/material';
+import {
+  Container,
+  TextField,
+  Button,
+  Typography,
+  Box,
+  Paper,
+  CircularProgress,
+} from '@mui/material';
 import WeatherIcon from './WeatherIcon'; // Custom component to display weather icons
+import { styled } from '@mui/material/styles';
 
 const API_KEY = '6eb9b4e26cfb2dfd8e6e9f9b4f7edfe4'; // Replace with your actual API key
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
+const CenteredContainer = styled(Container)({
+  minHeight: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(4),
+  maxWidth: 500,
+  width: '100%',
+  borderRadius: theme.spacing(2),
+  boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+}));
+
+const FormContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  '& > *': {
+    marginRight: theme.spacing(2),
+  },
+  [theme.breakpoints.down('sm')]: {
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    '& > *': {
+      marginBottom: theme.spacing(2),
+    },
+  },
+}));
+
+const WeatherContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  '& > *': {
+    margin: theme.spacing(1),
+  },
+}));
+
 const Temperature = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const fetchWeatherData = async () => {
+    setLoading(true);
     try {
       const response = await axios.get(API_URL, {
         params: {
@@ -20,8 +70,10 @@ const Temperature = () => {
         },
       });
       setWeatherData(response.data);
+      setLoading(false);
     } catch (error) {
       console.error('Error fetching weather data:', error);
+      setLoading(false);
     }
   };
 
@@ -35,43 +87,39 @@ const Temperature = () => {
   };
 
   return (
-    <Container maxWidth="sm">
-      <Box mt={5} p={3} component={Paper}>
+    <CenteredContainer>
+      <StyledPaper>
         <Typography variant="h4" gutterBottom>
           Weather Forecast
         </Typography>
-        <form onSubmit={handleSubmit}>
+        <FormContainer component="form" onSubmit={handleSubmit}>
           <TextField
             label="Enter city name"
             variant="outlined"
             fullWidth
             value={city}
             onChange={handleCityChange}
-            InputProps={{
-              endAdornment: (
-                <Button
-                  variant="contained"
-                  color="primary"
-                  size="large"
-                  type="submit"
-                >
-                  Get Weather
-                </Button>
-              ),
-            }}
           />
-        </form>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            type="submit"
+            disabled={!city || loading}
+          >
+            Get Weather
+          </Button>
+        </FormContainer>
+        {loading && <CircularProgress />}
         {weatherData && (
-          <Box mt={3}>
+          <WeatherContainer>
             <Typography variant="h5">
               {weatherData.name}, {weatherData.sys.country}
             </Typography>
-            <Box display="flex" alignItems="center">
-              <WeatherIcon iconCode={weatherData.weather[0].icon} />
-              <Typography variant="h4">
-                {Math.round(weatherData.main.temp)}°C
-              </Typography>
-            </Box>
+            <WeatherIcon iconCode={weatherData.weather[0].icon} />
+            <Typography variant="h4">
+              {Math.round(weatherData.main.temp)}°C
+            </Typography>
             <Typography variant="body1" gutterBottom>
               {weatherData.weather[0].description}
             </Typography>
@@ -87,10 +135,10 @@ const Temperature = () => {
             <Typography variant="body1">
               Humidity: {weatherData.main.humidity}%
             </Typography>
-          </Box>
+          </WeatherContainer>
         )}
-      </Box>
-    </Container>
+      </StyledPaper>
+    </CenteredContainer>
   );
 };
 
--- src/component/routes/WeatherIcon.js
+++ src/component/routes/WeatherIcon.js
@@ -1,31 +1,10 @@
+// src/WeatherIcon.js
 import React from 'react';
-import { Icon } from '@mui/material';
 
 const WeatherIcon = ({ iconCode }) => {
-  const iconMap = {
-    '01d': 'wb_sunny',
-    '01n': 'nights_stay',
-    '02d': 'day_sunny_overcast',
-    '02n': 'nights_stay',
-    '03d': 'cloud',
-    '03n': 'cloud',
-    '04d': 'cloudy',
-    '04n': 'cloudy',
-    '09d': 'showers',
-    '09n': 'showers',
-    '10d': 'day_rain',
-    '10n': 'night_rain',
-    '11d': 'thunderstorm',
-    '11n': 'thunderstorm',
-    '13d': 'snow',
-    '13n': 'snow',
-    '50d': 'fog',
-    '50n': 'fog',
-  };
+  const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
 
-  const iconName = iconMap[iconCode] || 'wb_sunny';
-
-  return <Icon>{iconName}</Icon>;
+  return <img src={iconUrl} alt="Weather Icon" />;
 };
 
 export default WeatherIcon;
