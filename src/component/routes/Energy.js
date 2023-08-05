b02a5c09d2c5cbd48f636771173d4c39bd0582f4
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-08-06 01:22:54 +0300

    styled the energy savings component

--- src/component/routes/Energy.js
+++ src/component/routes/Energy.js
@@ -5,7 +5,7 @@ import './Energy.css'; // Import a separate CSS file for styling
 function Energy() {
   const [predictedSavings, setPredictedSavings] = useState(0);
   const [latestRooftopId, setLatestRooftopId] = useState(null);
-  const [rooftopDetails, setrooftopDetails] = useState('');
+  const [rooftopDetails, setRooftopDetails] = useState('');
 
   useEffect(() => {
     fetchLatestRooftopId();
@@ -14,7 +14,7 @@ function Energy() {
   useEffect(() => {
     if (latestRooftopId !== null) {
       fetchEnergySavings(latestRooftopId);
-      fetchrooftopDetails(latestRooftopId);
+      fetchRooftopDetails(latestRooftopId);
     }
   }, [latestRooftopId]);
 
@@ -36,28 +36,48 @@ function Energy() {
     }
   };
 
-  const fetchrooftopDetails = async (rooftopId) => {
+  const fetchRooftopDetails = async (rooftopId) => {
     try {
       const response = await axios.get(`http://localhost:8080/api/rooftop/${rooftopId}`);
-      setrooftopDetails(response.data);
+      setRooftopDetails(response.data);
     } catch (error) {
       console.error('Error fetching rooftop details:', error);
     }
   };
 
+  // Calculate monthly and annual energy savings in kWh
+  const monthlySavings = predictedSavings;
+  const annualSavings = predictedSavings * 12;
+
+  // Assuming the cost of electricity per kWh is $0.12 (you can change this value as needed)
+  const electricityCostPerKWh = 0.12;
+  const monthlyCostSavings = monthlySavings * electricityCostPerKWh;
+  const annualCostSavings = annualSavings * electricityCostPerKWh;
+
   return (
     <div className="energy-container">
       <h1 className="energy-title">Energy Savings Calculator</h1>
-      {latestRooftopId === null || rooftopDetails === null ? (
+      {latestRooftopId === null || rooftopDetails === '' ? (
         <p className="energy-loading">Loading...</p>
       ) : (
         <div className="energy-details">
           <p className="energy-text">Rooftop Area: {rooftopDetails.area} Square Meters</p>
           <p className="energy-text">Rooftop Material: {rooftopDetails.material}</p>
           {predictedSavings > 0 ? (
-            <p className="energy-savings-positive">
-              Predicted Savings: {predictedSavings.toFixed(2)} kWh
-            </p>
+            <div>
+              <p className="energy-savings-positive">
+                Predicted Savings: {monthlySavings.toFixed(2)} kWh
+              </p>
+              <p className="energy-savings-positive">
+                Annual Savings: {annualSavings.toFixed(2)} kWh
+              </p>
+              <p className="energy-savings-positive">
+                Monthly Cost Savings: ${monthlyCostSavings.toFixed(2)}
+              </p>
+              <p className="energy-savings-positive">
+                Annual Cost Savings: ${annualCostSavings.toFixed(2)}
+              </p>
+            </div>
           ) : (
             <p className="energy-savings-negative">
               No energy savings data available for the latest rooftop.
